8aebbaa0a90fdcdb461f51d7f13a9592d16c4044
Maqsudkhan <maqsudtorayev2003@gmail.com>
2024-05-11 18:47:03 +0500

    Add UI components

---
+++ src/app/components/UI/homework-pop-up/homework-pop-up.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homework-pop-up',
+  templateUrl: './homework-pop-up.component.html',
+  styleUrl: './homework-pop-up.component.scss'
+})
+export class HomeworkPopUpComponent {
+
+}
