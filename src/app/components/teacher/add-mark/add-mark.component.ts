5e81079f34e90e6c05cdd07d5bb9996211a45284
Maqsudkhan <maqsudtorayev2003@gmail.com>
2024-05-11 18:07:53 +0500

    Append teacher and auth components

---
+++ src/app/components/teacher/add-mark/add-mark.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-mark',
+  templateUrl: './add-mark.component.html',
+  styleUrl: './add-mark.component.scss'
+})
+export class AddMarkComponent {
+
+}
