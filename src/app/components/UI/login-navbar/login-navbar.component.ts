8c8cfcdb7a5fdbab05ad63260c8d01e865187252
Abu_Programmiy <abdukholiq23@gmail.com>
2024-05-12 21:09:50 +0500

    login page tayyot

---
+++ src/app/components/UI/login-navbar/login-navbar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-navbar',
+  templateUrl: './login-navbar.component.html',
+  styleUrl: './login-navbar.component.scss'
+})
+export class LoginNavbarComponent {
+}
