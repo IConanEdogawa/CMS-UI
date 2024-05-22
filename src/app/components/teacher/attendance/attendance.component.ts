4f131ef6c94fadc8abe7b3c93fed44727c6ea26b
tohirjon-odilov <shahanshoh819@gmail.com>
2024-05-22 20:41:18 +0500

    working attendance class and lesson

--- src/app/components/teacher/attendance/attendance.component.ts
+++ src/app/components/teacher/attendance/attendance.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClassService } from '../../../services/class.service';
 
 @Component({
   selector: 'app-attendance',
   templateUrl: './attendance.component.html',
   styleUrl: './attendance.component.scss'
 })
-export class AttendanceComponent {
+export class AttendanceComponent implements OnInit {
+  classList: any;
+  lessonList: any;
+  constructor(private classService:ClassService) {
+    this.classService
+      .GetAllClasses().subscribe((res) => {
+        console.log(res);
+        this.classList = res
+      });
+  }
 
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {}
+
+  onFileSelected(event: any) {}
+
+  onChangeClass(event: any) {
+    console.log(event.target.value);
+    this.classService
+      .GettAllLessons().subscribe((res) => {
+        this.lessonList = res
+        console.log(this.lessonList);
+      });
+  }
+
+  onChangeLesson(event: any) {
+    console.log(event.target.value);
+  }
 }
--- src/app/services/class.service.ts
+++ src/app/services/class.service.ts
@@ -1,9 +1,24 @@
+import { environment } from './../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClassService {
 
-  constructor() { }
+  constructor(private readonly http: HttpClient) { }
+
+  baseUrl: string = environment.baseUrl
+  
+  GettAllLessons(): Observable<any> {
+    return this.http
+      .get<any>(this.baseUrl + `Lesson/GetAllLessons`)
+  }
+
+  GetAllClasses(): Observable<any> {
+    return this.http
+      .get<any>(this.baseUrl + `Class/GetAllClasses`)
+  } 
 }
