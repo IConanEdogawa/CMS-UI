a9e63a39c3d6d29a3c0ea4d4cfb486177c7f256b
Abu_Programmiy <abdukholiq23@gmail.com>
2024-05-23 10:45:38 +0500

    bootstrap yana qaydi

--- src/app/services/class.service.ts
+++ src/app/services/class.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClassService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  create(body:any):Observable<any>{
+    return this.http.post<any>(environment.baseUrl+"/Class/CreateClass",body)
+  }
 }
