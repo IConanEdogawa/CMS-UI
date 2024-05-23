b75543d14d18defeae81a48bd21625a3578f60ef
tohirjon-odilov <shahanshoh819@gmail.com>
2024-05-23 11:13:19 +0500

    fixed bugs

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    baseUrl:"https://localhost:7204/api",
-    mainPath:"https://localhost:7204"
+    baseUrl:"https://cms-api.tohirjon.uz/api/",
+    mainPath:"https://cms-api.tohirjon.uz/"
     // serverPath:"https://cms-api.tohirjon.uz/api"
 };
