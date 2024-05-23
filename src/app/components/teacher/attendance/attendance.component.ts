6df2b4c8c08c7a69187c36c1bfe568090e920267
tohirjon-odilov <shahanshoh819@gmail.com>
2024-05-23 10:45:09 +0500

    working attendance

--- src/app/components/teacher/attendance/attendance.component.ts
+++ src/app/components/teacher/attendance/attendance.component.ts
@@ -1,38 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { ClassService } from '../../../services/class.service';
+import { LessonService } from '../../../services/lesson.service';
+import { StudentService } from '../../../services/student.service';
 
 @Component({
   selector: 'app-attendance',
   templateUrl: './attendance.component.html',
-  styleUrl: './attendance.component.scss'
+  styleUrl: './attendance.component.scss',
 })
 export class AttendanceComponent implements OnInit {
   classList: any;
   lessonList: any;
-  constructor(private classService:ClassService) {
-    this.classService
-      .GetAllClasses().subscribe((res) => {
-        console.log(res);
-        this.classList = res
-      });
+  classId!: number;
+  studentList!: any;
+  constructor(
+    private readonly classService: ClassService,
+    private readonly lessonService: LessonService,
+    private readonly studentService: StudentService
+  ) {
+    this.classService.GetAllClasses().subscribe((res) => {
+      console.log(res);
+      this.classList = res;
+    });
   }
 
   ngOnInit(): void {}
-
   ngAfterViewInit(): void {}
-
   onFileSelected(event: any) {}
 
   onChangeClass(event: any) {
     console.log(event.target.value);
-    this.classService
-      .GettAllLessons().subscribe((res) => {
-        this.lessonList = res
-        console.log(this.lessonList);
-      });
+    this.classId = event.target.value;
+    this.lessonService.GetByIdLesson(event.target.value).subscribe((res) => {
+      this.lessonList = res;
+      console.log(this.lessonList);
+    });
   }
 
   onChangeLesson(event: any) {
     console.log(event.target.value);
+
+    this.studentService
+      .StudentGetById(this.classId).subscribe((res) => {
+        this.studentList = res;
+        console.log(this.studentList);
+      });
   }
 }
--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -1,20 +1,18 @@
-import { environment } from './../../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ClassService {
+export class StudentService {
 
   constructor(private readonly http: HttpClient) { }
 
   baseUrl: string = environment.baseUrl
-  
 
-  GetAllClasses(): Observable<any> {
-    return this.http
-      .get<any>(this.baseUrl + `Class/GetAllClasses`)
-  } 
+  StudentGetById(classId: number): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}Student/GetStudentsByClassId?id=${classId}`)
+  }
 }
