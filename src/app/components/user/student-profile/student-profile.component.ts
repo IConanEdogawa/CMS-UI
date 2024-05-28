f3da5ea07d1deeb7ebcd12cba7c244fbc9bddd59
Abu_Programmiy <abdukholiq23@gmail.com>
2024-05-28 07:01:15 +0500

    pachti taayyor

--- src/app/components/user/student-profile/student-profile.component.ts
+++ src/app/components/user/student-profile/student-profile.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { jwtDecode } from 'jwt-decode';
+import { StudentService } from '../../../services/student.service';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-student-profile',
@@ -7,4 +10,67 @@ import { Component } from '@angular/core';
 })
 export class StudentProfileComponent {
 
+  constructor(private studentService:StudentService){
+    this.id=(jwtDecode(localStorage.getItem("accessToken")!) as any).id
+    this.getStudentById()
+  }
+
+  student!:any
+  path!:string
+  id!:string
+  hello: any
+  direction!:string
+
+  getStudentById(){
+    this.studentService.studentGetById(this.id).subscribe({
+      next:(data)=>{
+        this.student=data
+        console.log(data)
+        if(this.student){
+          if(this.student.photoPath){
+
+            this.path=environment.mainPath+String(this.student.photoPath)
+            console.log(this.path)
+          }
+        }
+
+
+      },
+      error:(err)=>{
+        console.log(err)
+      }
+    })
+  }
+
+
+
+  mainButton(dir:string){
+    if(dir=="edit"){
+      this.change()
+    }
+    else{
+      this.save()
+    }
+  }
+
+  save(){
+
+  }
+
+  change() {
+    console.log(this.id)
+    var wrapper = document.getElementById("allData") as HTMLElement
+    (document.getElementById("edit")as HTMLDivElement).innerHTML="Save"
+    console.dir(wrapper.children)
+
+    wrapper.children[0].children[1].innerHTML = `<input type='text' style='width: 100%; height:40px' value="${wrapper.children[0].children[1].textContent}"/>`
+    wrapper.children[1].children[1].innerHTML = `<input type='text' style='width: 100%; height:40px' value="${wrapper.children[1].children[1].textContent}"/>`
+    wrapper.children[2].children[1].innerHTML = `<input type='text' style='width: 100%; height:40px' value="${wrapper.children[2].children[1].textContent}"/>`
+    wrapper.children[3].children[1].innerHTML = `<input type='text' style='width: 100%; height:40px' value="${wrapper.children[3].children[1].textContent}"/>`
+    wrapper.children[4].children[1].innerHTML = `<input type='text' style='width: 100%; height:40px' value="${wrapper.children[4].children[1].textContent}"/>`
+    wrapper.children[5].children[1].innerHTML = `<input type='text' style='width: 100%; height:40px' value="${wrapper.children[5].children[1].textContent}"/>`
+    wrapper.children[6].children[1].innerHTML = `<input type='text' style='width: 100%; height:40px' value="${wrapper.children[6].children[1].textContent}"/>`
+    wrapper.children[7].children[1].innerHTML = `<input type='text' style='width: 100%; height:40px' value="${wrapper.children[7].children[1].textContent}"/>`
+
+  }
 }
--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -15,4 +15,13 @@ export class StudentService {
   StudentGetById(classId: number): Observable<any> {
     return this.http.get<any>(`${this.baseUrl}/Student/GetStudentsByClassId?id=${classId}`)
   }
+
+  studentGetById(id:string):Observable<any>{
+    return this.http.get(this.baseUrl+`/Student/GetStundetById?Id=${id}`)
+  }
+
+  studentUpdate(data:any):Observable<any>{
+    return this.http.put<any>(this.baseUrl+`Student/UpdateStudent?Id=${data.id}&FirsName=${data.firstName}&LastName=${data.lastName}&Gender=${data.gender}&DateOfBirth.Day=${data.day}&DateOfBirth.Month=${data.month}&DateOfBirth.Year=${data.year}&Location.Country=${data.location.country}&Location.Region=${data.location.region}&Location.District=${data.location.district}&Location.HomeNumber=${data.location.homeNumber}&ParentsPhoneNumber=${data.parentsPhoneNumber}`,{})
+  }
+
 }
